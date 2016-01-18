1ef666acf805a308f31d3d977785a496802aaa35
Kamil Qureshi <mkq001@gmail.com>
2016-01-18 20:35:42 +0500

    Chenges in association and tests

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -5,9 +5,10 @@ class RecipeTest < ActiveSupport::TestCase
 
 
 def setup
-@recipe = Recipe.new(:name => "Chicken Tikka",:summary => "BBQ Dish",:description => 
-	           			"Cook The dish on grill for 15 mins after marination")
+@chef = Chef.create(chefname: "Bob", email: "bob@example.com")	
 
+@recipe = @chef.recipes.build(:name => "Chicken Tikka",:summary => "BBQ Dish",:description => 
+	           			"Cook The dish on grill for 15 mins after marination")
 end
 
 
@@ -15,6 +16,12 @@ test "recipe should be valid" do
  assert_not @recipe.valid?
 end
 
+ 
+
+test "chef_id should be present" do 
+	@recipe.chef_id = nil
+	assert_not @recipe.valid?
+end	
 
 
 test "name should be present" do 
