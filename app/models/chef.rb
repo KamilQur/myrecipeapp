020bb4da292e744f8c3e986d19b87514768c70da
Kamil Qureshi <mkq001@gmail.com>
2016-01-21 03:02:07 +0500

    Builted forms partials

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -9,7 +9,7 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
 validates :email,presence:true,length:{maximum:105},
                             uniqueness: {case_sensitive: false},
-                            format:  {with: VALID_EMAIL_REGEX}
+                             format:  {with: VALID_EMAIL_REGEX}
 
 
                          
